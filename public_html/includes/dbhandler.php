4fbdff0440fc1ead085020a29e309cec8fd4c2c3
BrickBeard <brickbeard.io@gmail.com>
2018-08-24 09:11:40 -0500

    Commented out some error handling code that was being thrown for mysql command failures... whoops!

--- public_html/includes/dbhandler.php
+++ public_html/includes/dbhandler.php
@@ -13,12 +13,14 @@ $conn = mysqli_connect('localhost', 'root', '', 'authentication');
 
 if (!$conn) {
     /*die("Connection failed: " . $conn->connect_error);*/
-    die("Connection to database server failed: ".$mysqli_connect_error());
+    die("Connection to database server failed.");
 }
+/*
 if (!mysqli_set_charset($conn, 'utf8')) {
     die("Unable to set database connection encoding.");
 }
 if (!mysqli_select_db($conn, $database)) {
     die("Unable to locate the " . $database . " database." );
 }
+*/
 ?>
