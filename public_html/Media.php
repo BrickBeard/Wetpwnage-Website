13918fc96b50fef4f4591d6f92f684730ce06570
BrickBeard <brickbeard.io@gmail.com>
2018-08-27 11:01:16 -0500

    updated Media.php file

--- public_html/Media.php
+++ public_html/Media.php
@@ -32,43 +32,43 @@
                     <iframe src="https://events.google.com/io/embed" style="width:80%;height:auto;min-height:500px;margin-bottom:200px;" frameborder="0" allowfullscreen></iframe>
                 </div>
                 -->
-                <div class="container gamesContent">
-            <div class="row"> 
-                <div class="column">
-                    <h3><a href="#">SpankBall</a></h3>
-                    <img src="media/SpankBall/thumbnails/sbThumb01.png" style="width:100%">
-                    <img src="media/SpankBall/thumbnails/sbThumb02.png" style="width:100%">
-                    <img src="media/SpankBall/thumbnails/sbThumb04.png" style="width:100%">
-                    <img src="media/SpankBall/thumbnails/sbThumb05.png" style="width:100%">
-                    <img src="media/SpankBall/thumbnails/sbThumb06.png" style="width:100%">
-                    <img src="media/SpankBall/thumbnails/sbThumb07.png" style="width:100%">
+            <div class="container gamesContent">
+                <div class="row"> 
+                    <div class="column">
+                        <h3><a href="#">SpankBall</a></h3>
+                        <img src="media/SpankBall/thumbnails/sbThumb01.png" style="width:100%">
+                        <img src="media/SpankBall/thumbnails/sbThumb02.png" style="width:100%">
+                        <img src="media/SpankBall/thumbnails/sbThumb04.png" style="width:100%">
+                        <img src="media/SpankBall/thumbnails/sbThumb05.png" style="width:100%">
+                        <img src="media/SpankBall/thumbnails/sbThumb06.png" style="width:100%">
+                        <img src="media/SpankBall/thumbnails/sbThumb07.png" style="width:100%">
+                    </div>
+                    <div class="column">
+                        <h3><a href="#">TurdBird</a></h3>
+                        <img src="media/TurdBird/Images/BirdExplode.PNG" style="width:100%">
+                        <img src="media/TurdBird/Images/TurdBirdPic.PNG" style="width:100%">
+                        <img src="media/TurdBird/Images/TurdBirdScreenShot1.PNG" style="width:100%">
+                        <img src="media/TurdBird/Images/TurdBirdScreenShot2.PNG" style="width:100%">
+                    </div>  
+                    <div class="column">
+                        <h3><a href="#">RTS</a></h3>
+                        <img src="media/RTS/AnimalsHeavyUnits.png" style="width:100%">
+                        <img src="media/RTS/deer vs chicken.png" style="width:100%">
+                        <img src="media/RTS/foxes.png" style="width:100%">
+                        <img src="media/RTS/foxes3.png" style="width:100%">
+                        <img src="media/RTS/img_20180220.jpg" style="width:100%">
+                    </div>
+    <!--
+                    <div class="column">
+                        <img src="/w3images/underwater.jpg" style="width:100%">
+                        <img src="/w3images/ocean.jpg" style="width:100%">
+                        <img src="/w3images/wedding.jpg" style="width:100%">
+                        <img src="/w3images/mountainskies.jpg" style="width:100%">
+                        <img src="/w3images/rocks.jpg" style="width:100%">
+                        <img src="/w3images/underwater.jpg" style="width:100%">
+                    </div>
+    -->
                 </div>
-                <div class="column">
-                    <h3><a href="#">TurdBird</a></h3>
-                    <img src="media/TurdBird/Images/BirdExplode.PNG" style="width:100%">
-                    <img src="media/TurdBird/Images/TurdBirdPic.PNG" style="width:100%">
-                    <img src="media/TurdBird/Images/TurdBirdScreenShot1.PNG" style="width:100%">
-                    <img src="media/TurdBird/Images/TurdBirdScreenShot2.PNG" style="width:100%">
-                </div>  
-                <div class="column">
-                    <h3><a href="#">RTS</a></h3>
-                    <img src="media/RTS/AnimalsHeavyUnits.png" style="width:100%">
-                    <img src="media/RTS/deer vs chicken.png" style="width:100%">
-                    <img src="media/RTS/foxes.png" style="width:100%">
-                    <img src="media/RTS/foxes3.png" style="width:100%">
-                    <img src="media/RTS/img_20180220.jpg" style="width:100%">
-                </div>
-<!--
-                <div class="column">
-                    <img src="/w3images/underwater.jpg" style="width:100%">
-                    <img src="/w3images/ocean.jpg" style="width:100%">
-                    <img src="/w3images/wedding.jpg" style="width:100%">
-                    <img src="/w3images/mountainskies.jpg" style="width:100%">
-                    <img src="/w3images/rocks.jpg" style="width:100%">
-                    <img src="/w3images/underwater.jpg" style="width:100%">
-                </div>
--->
-            </div>
             </div>
         </div>
 <!---------- FOOTER ---------->
